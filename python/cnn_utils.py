fe448c48da9b4a603911f65ef12339c286df5afc
Patrick Ma <jupyter@tensorflow-1-20200617-140053.us-west1-b.c.integral-hybrid-280102.internal>
2020-06-23 01:08:38 +0000

    add fetch api

--- python/cnn_utils.py
+++ python/cnn_utils.py
@@ -4,11 +4,12 @@ import base64
 import cv2
 import random
 import matplotlib.pyplot as plt
+from PIL import Image
 import pandas as pd
 import numpy as np
 import os.path
 
-def create_dataframe():
+def create_dataframe(relative_dir ='/../raw_data/dataset_062120.json' ):
     """
     convert the JSON file of my research into a dataframe with the following columns:
     eyeImage: np array with shape (25, 50, 3)
@@ -16,7 +17,38 @@ def create_dataframe():
     rightEye: np array with shape (6,2): 6 right eye landmarks positions in the form of (x,y), where -1<=x,y<=1
     y: the position that the user looks at on the screen in the form of (x,y), where -1<=x,y<=1
     """
-    f = open(os.path.join(os.path.dirname(__file__))+'/../dataset.json')
+    f = open(os.path.join(os.path.dirname(__file__))+relative_dir)
+    eyeImages,leftEyes,rightEyes,ys = [], [], [], []
+    for item in ijson.items(f, "item"):
+        # convert the string into a python dict
+    
+        temp = json.dumps(item)
+        temp = json.loads(temp) 
+        # convert the image into the np array
+        eyeImage = convert_base64_to_nparray(temp["x"]["eyeImage"])
+        eyeImages.append(eyeImage)
+
+        leftEye = np.array(temp["x"]["eyePositions"]["leftEye"]).reshape(12)
+        leftEyes.append(leftEye)
+        rightEye = np.array(temp["x"]["eyePositions"]["rightEye"]).reshape(12)
+        rightEyes.append(rightEye)
+
+        y = np.array(temp["y"])
+        ys.append(y)
+
+    list_of_tuples = list(zip(eyeImages,leftEyes,rightEyes,ys))
+    df = pd.DataFrame(list_of_tuples, columns =['eyeImage','leftEye', 'rightEye', 'y'])
+    return df
+
+def create_dataframe(relative_dir ='/../raw_data/dataset_062120.json' ):
+    """
+    convert the JSON file of my research into a dataframe with the following columns:
+    eyeImage: np array with shape (25, 50, 3)
+    leftEye: np array with shape (6,2): 6 left eye landmarks positions in the form of (x,y), where -1<=x,y<=1
+    rightEye: np array with shape (6,2): 6 right eye landmarks positions in the form of (x,y), where -1<=x,y<=1
+    y: the position that the user looks at on the screen in the form of (x,y), where -1<=x,y<=1
+    """
+    f = open(os.path.join(os.path.dirname(__file__))+relative_dir)
     eyeImages,leftEyes,rightEyes,ys = [], [], [], []
     for item in ijson.items(f, "item"):
         # convert the string into a python dict
@@ -37,7 +69,7 @@ def create_dataframe():
     df = pd.DataFrame(list_of_tuples, columns =['eyeImage','leftEye', 'rightEye', 'y'])
     return df
 
-def create_train_validation(df, train_percentage=0.7):
+def create_train_validation(df, train_percentage=0.8):
     """
     Take in original dataframe and split it into train&validation set
     Follow the 70/30 convention
@@ -63,6 +95,33 @@ def convert_base64_to_nparray(image_data):
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)/255.
     return image
 
+
+def save_jpgs(relative_dir ='/../raw_data/dataset_062120.json' ):
+    """
+    convert the JSON file of my research into a dataframe with the following columns:
+    eyeImage: np array with shape (25, 50, 3)
+    leftEye: np array with shape (6,2): 6 left eye landmarks positions in the form of (x,y), where -1<=x,y<=1
+    rightEye: np array with shape (6,2): 6 right eye landmarks positions in the form of (x,y), where -1<=x,y<=1
+    y: the position that the user looks at on the screen in the form of (x,y), where -1<=x,y<=1
+    """
+    f = open(os.path.join(os.path.dirname(__file__))+relative_dir)
+    i = 0
+    for item in ijson.items(f, "item"):
+        # convert the string into a python dict
+        temp = json.loads(item) 
+        # convert the image into the np array
+        eyeImage = save_nparray_to_jpg(temp["x"]["eyeImage"],i)
+        i += 1
+
+def save_nparray_to_jpg(image_data,idx):
+    image = image_data.split(",")[1]
+    image = np.asarray(bytearray(base64.b64decode(image)), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    im = Image.fromarray(image)
+    im.save("image_data/"+str(idx)+".jpeg")
+
+    
+
 def create_binary_labels(y_labels):
     """
     takes in our original y labels, each label is in the form of [x,y]
@@ -82,10 +141,10 @@ def create_quaternary_labels(y_labels):
     """
     takes in our original y labels, each label is in the form of [x,y]
     convert the original labels into a new y labels which has the following encoding:
-        UR: upperright, where x>0, y<=0
-        UL: upperleft, where x<=0, y<=0
-        LR: lowerright, where x>0, y>0
-        LL: lowerleft, where x<=0, y>0
+        0: upperleft, where x<=0, y<=0
+        1: upperright, where x>0, y<=0
+        2: lowerright, where x>0, y>0
+        3: lowerleft, where x<=0, y>0
     input:
         y_labels: a 1*n pandas df with its element in the form of [x,y]
     output:
@@ -94,8 +153,8 @@ def create_quaternary_labels(y_labels):
     quanternary_labels = pd.DataFrame().reindex_like(y_labels)
     isRight = y_labels["y"].map(lambda x: x[0]) > 0
     isLower = y_labels["y"].map(lambda x: x[1]) > 0
-    quanternary_labels.loc[isRight & ~isLower] = "UR"
     quanternary_labels.loc[~isRight & ~isLower] = "UL"
+    quanternary_labels.loc[isRight & ~isLower] = "UR"
     quanternary_labels.loc[isRight & isLower] = "LR"
     quanternary_labels.loc[~isRight & isLower] = "LL"
     return quanternary_labels
@@ -117,6 +176,52 @@ def plot_eyeImages(X, Y):
     axes = axes.flatten()
     for img, ax, label, index in zip(images_arr, axes, labels_arr, indexes):
         ax.imshow(img)
-        ax.set_title("idx: "+str(index)+" label: "+label,fontdict = {'fontsize': 20})
+        ax.set_xlabel("idx: "+str(index)+" label: "+label,fontdict = {'fontsize': 20})
     plt.tight_layout()
     plt.show()
+    
+def random_mini_batches(eyeImage, leftEye, rightEye, Y, mini_batch_size = 32, seed = 0):
+    """
+    Creates a list of random minibatches from (X, Y)
+    
+    Arguments:
+    X -- input data, of shape (input size, number of examples) (m, Hi, Wi, Ci)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples) (m, n_y)
+    mini_batch_size - size of the mini-batches, integer
+    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
+    
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+    
+    m = eyeImage.shape[0]                  # number of training examples
+    mini_batches = []
+    np.random.seed(seed)
+    
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_eyeImage = eyeImage[permutation,:,:,:]
+    shuffled_leftEye = leftEye[permutation,:,:,:]
+    shuffled_rightEye = rightEye[permutation,:,:,:] 
+    shuffled_Y = Y[permutation]
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_eyeImage = shuffled_eyeImage[k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch_leftEye = shuffled_leftEye[k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch_rightEye = shuffled_rightEye[k * mini_batch_size : k * mini_batch_size + mini_batch_size]        
+        mini_batch_Y = shuffled_Y[k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_eyeImage, mini_batch_leftEye, mini_batch_rightEye, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_eyeImage = shuffled_eyeImage[num_complete_minibatches * mini_batch_size : m]
+        mini_batch_leftEye = shuffled_leftEye[num_complete_minibatches * mini_batch_size : m]
+        mini_batch_rightEye = shuffled_rightEye[num_complete_minibatches * mini_batch_size : m]
+        mini_batch_Y = shuffled_Y[num_complete_minibatches * mini_batch_size : m,:]
+        mini_batch = (mini_batch_eyeImage, mini_batch_leftEye, mini_batch_rightEye, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
